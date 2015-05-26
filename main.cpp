8b4bebb3e5b00b5bc8b5048112c2f57974f8e6a0
JunoIm <imjunera@gmail.com>
2015-05-26 22:23:54 +0900

    소켓까지
    ㅇㅇ

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 #include<time.h>
 #include<string.h>
+#pragma comment(lib,"Ws2_32.lib")
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include<Windows.h>
 
 /*
@@ -32,7 +36,7 @@ void makePokemon(orderPokemon *op) {
 
 	int i, j;
 	int randValue;
-	char namesPokemon[20][20] = { { "치코리타" }, { "꼬부기" }, { "피카츄" }, { "잉어킹" }, { "펭도리" }, { "파이리" }, { "장동건" }, { "고유빈" }, { "김동현" }, { "김준기" }, { "네퓨즈" } };
+	char namesPokemon[20][20] = { { "치코리타" }, { "꼬부기" }, { "피카츄" }, { "잉어킹" }, { "펭도리" }, { "파이리" }, { "장동건" }, { "고유빈" }, { "김동현" }, { "임준오" }, { "네퓨즈" } };
 	int healthesPokemon[20] = { 500, 480, 450, 520, 420, 500, 510, 530, 520, 510, 10 };
 
 	for (i = 0; i<6; i++) {
@@ -137,13 +141,19 @@ int isNotDuplicateID(char *userID, int line) {
 	return 1;
 }
 
+int socketError() {
+	printf("윈속 생성 오류 !!\n프로그램을 종료합니다 ㅠㅠ\n");
+	return 0;
+}
 
 int main() {
 	char userID[20], userPW[20], fileUserID[20], fileUserPW[20];
-	char LoginOrJoin;
-	int checkFlag = 0, checkUserDuplicate = 0, line;
+	char readBuffer[30],sendBuffer[30]="임준오";
+	int port, sizeClientAddr;;
+	char LoginOrJoin, MakeOrJoin;
+	int checkFlag = 0, checkUserDuplicate = 0, checkRead = 0, line;
 	int i;
-	
+
 	line = countLine();
 
 	while (checkFlag == 0) {
@@ -163,7 +173,7 @@ int main() {
 		if (LoginOrJoin == '1' && isLoginSuccess(userID,userPW,line)) {
 			printf("로그인 성공 !! \n");
 			checkFlag = 1;
-			Sleep(1000);
+			//Sleep(1000);
 			system("cls");
 			printf("총 승리 : %d 총 패배 : %d\n", countWin, countLose);
 		}
@@ -178,21 +188,153 @@ int main() {
 		else {
 			printf("실패 !! \n");
 		}
-
 	}
 
 	orderPokemon* Dawn = (orderPokemon*)malloc(sizeof(orderPokemon));
 
 	printf("****** 포켓몬 리스트 초기화중 ... ******\n");
+
 	initPokemon(Dawn);
-	Sleep(1000);
+	//Sleep(1000);
+
 	makePokemon(Dawn);
+
 	printf("****** 포켓몬 생성중 ... ******\n");
-	Sleep(1500);
+	//Sleep(1500);
 	printf("****** 포켓몬 생성완료 !!! ******\n");
 	printf("****** 당신의 6마리 포켓몬입니다. ******\n");
-	printAllPokemon(Dawn);
-	printf("****** 스킬 배정중입니다.. ******\n");
 
+	printAllPokemon(Dawn);
+
+	printf("****** 스킬 배정중입니다.. ******\n");
+	printf("****** 스킬 배정실패 !!! ******\n");
+	printf("[Notice] : 당신의 플레이시간이 1억시간 미만이므로\n스킬은 몸통박치기 밖에 지원을 하지않습니다.\n");
+	printf("프리미엄으로 업그레이드시 사용가능합니다. 업글 비용 \\9,912,323,200원\n");
+	printf("352 0273 9941 13 농협 임준오\n");
+
+	printf("\n[1] 방생성하기\n");
+	printf("[2] 방참여하기\n");
+	printf("=> ");
+	fflush(stdin);
+	scanf("%c", &MakeOrJoin);
+
+	SOCKET pokemonClient = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET pokemonServer = socket(AF_INET, SOCK_STREAM, 0);
+	WSADATA wsa;
+	SOCKADDR_IN serverAddr;
+	SOCKADDR_IN clientAddr;
+
+	if (MakeOrJoin == '1') {
+		printf("Input Port Number ! => ");
+		scanf("%d", &port);
+		WSADATA wsa; // init Winsock
+
+		if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0){
+			socketError();
+		}
+
+		if (pokemonServer == INVALID_SOCKET || pokemonClient == INVALID_SOCKET)
+			socketError();
+
+		memset(&serverAddr, 0, sizeof(serverAddr));
+		serverAddr.sin_family = AF_INET;
+		serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+		serverAddr.sin_port = htons(port);
+
+		int status;
+
+		status = bind(pokemonServer, (struct sockaddr*)&serverAddr, sizeof(struct sockaddr_in));
+		if (status == SOCKET_ERROR)
+		{
+			printf("Bind Error\n");
+			return 0;
+		}
+		if (SOCKET_ERROR == listen(pokemonServer, 5))
+		{
+			printf("listen Error\n");
+			return 0;
+		}
+		printf("연결 대기중 .. 포트 : %d\n", port);
+
+		while (1) {
+			sizeClientAddr = sizeof(clientAddr);
+			pokemonClient = accept(pokemonServer, (SOCKADDR*)&clientAddr, &sizeClientAddr);
+
+			if (pokemonClient == INVALID_SOCKET) {
+				closesocket(pokemonClient);
+				closesocket(pokemonServer);
+				socketError();
+			}
+
+			printf("클리언트와 연결됨 !!\n");
+			checkRead = recv(pokemonClient, readBuffer, 30, 0);
+			readBuffer[checkRead-1] = NULL;
+			printf("상대는 ~!! %s !! 초고수입니다 ~\n", readBuffer);
+			if (checkRead > 0) {
+				strcpy(sendBuffer, userID);
+				send(pokemonClient, sendBuffer, sizeof(sendBuffer), 0); // 다시 닉네임 보내기
+			}
+			else {
+				printf("Error !!!!!!!!!!!\n");
+				getch();
+				socketError();
+			}
+			checkRead = recv(pokemonClient, readBuffer, 30, 0);
+			readBuffer[checkRead] = NULL;
+			printf("%s", readBuffer);
+		}
+		closesocket(pokemonClient);
+		closesocket(pokemonServer);
+	}
+	else if (MakeOrJoin == '2') { // 클라이언트 부분
+		char serverIP[30];
+		int serverPort;
+
+		printf("Input => IP PORT ");
+		scanf("%s %d", serverIP, &serverPort);
+
+		if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0){
+			socketError();
+		}
+		if ((pokemonClient = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+			socketError();
+		}
+
+		memset((void *)&serverAddr, 0x00, sizeof(serverAddr));
+		serverAddr.sin_family = AF_INET;
+		serverAddr.sin_addr.s_addr = inet_addr(serverIP);
+		serverAddr.sin_port = htons(serverPort);
+
+		if (connect(pokemonClient, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+			socketError();
+		}
+
+		send(pokemonClient, sendBuffer, strlen(sendBuffer), 0);
+
+		while (1)
+		{
+			printf("> ");
+			fgets(sendBuffer, 29, stdin);
+			if (strncmp(sendBuffer, "quit\n", 5) == 0)
+			{
+				break;
+			}
+			send(pokemonClient, sendBuffer, strlen(sendBuffer), 0);
+			memset(readBuffer, 0x00, 30);
+			recv(pokemonClient, readBuffer, 30, 0);
+			printf("s -> %s\n", readBuffer);
+		}
+
+		closesocket(pokemonClient);
+	}
+
+	//유저정보 받고 소켓 받는거 끝났으면 ㄱㄱ !!
+	send(pokemonClient, sendBuffer, strlen(sendBuffer), 0);
+	memset(readBuffer, 0x00, 30);
+	recv(pokemonClient, readBuffer, 30, 0);
+	send(pokemonClient, sendBuffer, sizeof(sendBuffer), 0); // 다시 닉네임 보내기
+	checkRead = recv(pokemonClient, readBuffer, 30, 0);
+
+	WSACleanup();
 	return 1;
 }
